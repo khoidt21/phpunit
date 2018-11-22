dffdf6c3531219073e7251aa7706acbd9f1e3ee5
Doan Tuan Khoi <tkhoi16@yahoo.com>
2018-11-23 05:29:38 +0700

    change password in function User

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <script>
         function User(){
             this.username = 'Khoi';
-            this.password = '';
+            this.password = '123';
             this.address = '';
         }
 </script>
