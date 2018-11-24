df6559843d07ca19164fca002f78c66c3315c17e
Doan Tuan Khoi <tkhoi16@yahoo.com>
2018-11-24 08:57:46 +0700

    Khoidt21 test.js

---
+++ test.js
@@ -0,0 +1,50 @@
+/*
+function User(){
+       
+        var username = '';
+        var password = '';
+        
+        this.setInfo = function (username,password){
+            this.username = username;
+            this.password = password;
+        }
+        this.checkLogin = function(){
+            return (this.username === 'admin' && this.password === '@123');
+        }
+        return this;
+}
+var user = new User();
+user.setInfo('admin','@123');
+if(user.checkLogin()){
+    console.log("Login scuess");
+}
+else{
+    console.log("Login Error");
+}
+*/
+function User(){
+     
+    this.username = '';
+    this.password = '';
+
+    User.prototype.setInfo = function(username,password){
+        this.username = username;
+        this.password = password;
+    }
+    User.prototype.checkLogin = function(){
+        return (this.username === 'admin' && this.password === '@123');
+    }
+    return this;
+}
+var user = new User();
+user.setInfo('admin','@123');
+if(user.checkLogin()){
+    console.log("Login scuess");
+}
+else{
+    console.log("Login error");
+}
+
+
+
+
