dc0df30d49ca58e60ee89fd672b2c12916519ac9
Doan Tuan Khoi <tkhoi16@yahoo.com>
2018-11-24 07:31:45 +0700

    change index setInfo and checkLogin

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
         function User(){
             this.username = '';
             this.password = '';
-            
+            // phuong thuc
             this.setInfo(username,password){
                 this.username = username;
                 this.password = password;
             }
+            this.checkLogin(){
+                return (this.username === 'admin' && this.password === '@123');
+            }
+        }
+        var user = new User();
+        user.setInfo('admin','@123');
+        if(user.checkLogin()){
+            console.log("Dang nhap thanh cong.");
+        }
+        else{
+            console.log("Dang nhap that bai.");
         }
 </script>
